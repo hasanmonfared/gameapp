177713ad0fac446f1db4ea9970a258b38a3f15c2
hasanmonfared <hasandoulabi@gmail.com>
2024-01-23 16:34:02 +0330

    completed websocket

--- cmd/example/websocket/main.go
+++ cmd/example/websocket/main.go
@@ -1,34 +1,34 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"gameapp/entity"
-	"gameapp/pkg/protobufencoder"
 	"github.com/labstack/gommon/log"
 	"net"
 	"net/http"
+	"time"
 
 	"github.com/gobwas/ws"
 	"github.com/gobwas/ws/wsutil"
 )
 
-func printDecodedNotification() {
-	d := protobufencoder.EncodeNotification(entity.Notification{
-		EventType: "ping",
-		Payload:   "hello",
-	})
-	fmt.Println("d", d)
-}
 func main() {
-	printDecodedNotification()
 	http.ListenAndServe(":8080", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		conn, _, _, err := ws.UpgradeHTTP(r, w)
 		if err != nil {
 			// handle error
 		}
+
 		defer conn.Close()
+
+		channel := make(chan string)
+		go producer(r.RemoteAddr, channel)
+		go writeMessage(conn, channel)
+
 		done := make(chan bool)
 		go readMessage(conn, done)
+
 		<-done
 	}))
 }
@@ -38,9 +38,30 @@ func readMessage(conn net.Conn, done chan<- bool) {
 		if err != nil {
 			log.Print(err)
 			done <- true
+			return
 		}
-		notif := protobufencoder.DecodeNotification(string(msg))
+		var notif = entity.Notification{}
+		err = json.Unmarshal(msg, &notif)
+		if err != nil {
+			panic(err)
+		}
 		fmt.Println("opCode", opCode)
 		fmt.Println("notif", notif)
+	}
+}
+func producer(remoteAddress string, channel chan<- string) {
+	for {
+		channel <- remoteAddress
+		time.Sleep(2 * time.Second)
+	}
+}
+func writeMessage(conn net.Conn, channel <-chan string) {
+	for data := range channel {
+
+		err := wsutil.WriteServerMessage(conn, ws.OpText, []byte(data))
+		if err != nil {
+			panic(err)
+		}
+
 	}
 }
